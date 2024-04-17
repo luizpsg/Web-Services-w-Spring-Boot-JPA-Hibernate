cb0cff37c9a8b22b69a4de6fde214aff6f88cf5c
luizpsg <luiz.saud@sga.pucminas.br>
2024-04-17 19:55:10 -0300

    Add DatabaseException class

--- course/src/main/java/com/luiz/course/services/exceptions/ResourceNotFoundException.java
+++ course/src/main/java/com/luiz/course/services/exceptions/ResourceNotFoundException.java
@@ -1,11 +1,10 @@
 package com.luiz.course.services.exceptions;
 
-public class ResourceNotFoundException extends RuntimeException{
+public class ResourceNotFoundException extends RuntimeException {
 
-  private static final long serialVersionUID = 1L;
-  
-  public ResourceNotFoundException(Object id) {
-    super("Resource not found. Id " + id);
-  }
+	private static final long serialVersionUID = 1L;
 
+	public ResourceNotFoundException(Object id) {
+		super("Resource not found. Id " + id);
+	}
 }
